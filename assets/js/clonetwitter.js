ce008cf26c83bfb3e37f30a21f1a176663f6e291
Shirley Silvana Suarez Startary <Shirley Silvana Suarez Startary>
2017-04-04 07:34:16 -0500

    resolviendo contador de clicks, falta estilo, y que cambie el nro de color el contador de caracteres

--- assets/js/clonetwitter.js
+++ assets/js/clonetwitter.js
@@ -7,7 +7,11 @@ var areaDeComentario=document.getElementById("comentario");
 areaDeComentario.addEventListener("keydown",cuentaCaracteres);
 areaDeComentario.addEventListener("keyup",cuentaCaracteres);
 
+areaDeComentario.addEventListener("click",noClicks);
+document.getElementById("autor").addEventListener("click",noClicks);
+document.getElementById("botonEnviar").addEventListener("click",noClicks);
 document.addEventListener("click",cuentaClicks);
+
 
 function Publicaciones(comentario,autor){
   this.comentario=comentario;
@@ -24,15 +28,18 @@ function crearComentario(){
 
   var articulo=document.createElement("article");
   var nombreAutor=document.createElement("span");
+  var linea=document.createElement("hr");
 
   seccionComentarios.appendChild(articulo);
   seccionComentarios.appendChild(nombreAutor);
+  seccionComentarios.appendChild(linea);
 
   articulo.innerText="Twita: "+comentario;
   nombreAutor.innerText="Por: "+autor;
 
   var publicacion =new Publicaciones(comentario,autor);
   comentarios.push(publicacion);
+
 
   console.log(publicacion);
 
@@ -43,11 +50,16 @@ function cuentaCaracteres(){
   var contador=document.getElementById("contador")
   var comentario = document.getElementById("comentario");
   contador.innerText=comentario.value.length+"/140";
-  console.log(contador);
+  //console.log(contador);
 }
 
 function cuentaClicks(){
   contadorCliks++
   var muestraClicks=document.getElementById("muestraClicks");
-  muestraClicks.innerText=contadorCliks;
+  muestraClicks.innerText="contador de Twitas: "+contadorCliks;
+  //event.stopPropagation();
+}
+
+function noClicks(){
+  event.stopPropagation();
 }
